3330a86c66aeab8e93a625102e7946718f9b1c2d
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-01 18:58:36 +0200

    feat(commands): ajout de la commande `/rtp reload` pour recharger la configuration du plugin

--- src/main/java/fr/jessee/firstSpawnRTP/command/sub/Reload.java
+++ src/main/java/fr/jessee/firstSpawnRTP/command/sub/Reload.java
@@ -1,4 +1,43 @@
 package fr.jessee.firstSpawnRTP.command.sub;
 
-public class Reload {
+import fr.jessee.firstSpawnRTP.FirstSpawnRTP;
+import fr.jessee.firstSpawnRTP.feature.AbstractSubCommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class Reload extends AbstractSubCommand {
+    @Override
+    public String getName() {
+        return "reload";
+    }
+    @Override
+    public List<String> getAliases() {
+        return List.of("");
+    }
+    @Override
+    public String getDescription() {
+        return "Reload the plugin configuration";
+    }
+    @Override
+    public String getUsage() {
+        return "/rtp reload";
+    }
+
+    @Override
+    public void executePlayer(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player p)) return;
+
+        FirstSpawnRTP.getConfigFile().reload();
+        p.sendMessage(FirstSpawnRTP.getLangFile().getString("reload_success"));
+    }
+
+    @Override
+    public void executeConsole(CommandSender sender, String[] args) {
+        if (sender instanceof Player) return;
+
+        FirstSpawnRTP.getConfigFile().reload();
+        sender.sendMessage(FirstSpawnRTP.getLangFile().getString("reload_success"));
+    }
 }
