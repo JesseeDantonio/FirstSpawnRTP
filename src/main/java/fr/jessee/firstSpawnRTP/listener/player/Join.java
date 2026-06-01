9273a8ea9e3d9d462db0a03dfcf480a3724a1a36
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-01 18:49:46 +0200

    feat(player-join): Ajout d'un écouteur de connexion pour le rtp

--- src/main/java/fr/jessee/firstSpawnRTP/listener/player/Join.java
+++ src/main/java/fr/jessee/firstSpawnRTP/listener/player/Join.java
@@ -1,4 +1,26 @@
 package fr.jessee.firstSpawnRTP.listener.player;
 
-public class Join {
+import fr.jessee.firstSpawnRTP.FirstSpawnRTP;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class Join implements Listener {
+
+    @EventHandler
+    public void onJoin(@NotNull final PlayerJoinEvent EVENT) {
+        final Player PLAYER = EVENT.getPlayer();
+        final UUID UUID_P = PLAYER.getUniqueId();
+
+        FirstSpawnRTP.getInstance().getPlayerRepository().getIsPlayerExistAsync(UUID_P).thenAccept(playerExist -> {
+            if (!playerExist) {
+                FirstSpawnRTP.getInstance().getPlayerRepository().getAddAsync(UUID_P);
+            }
+        });
+    }
 }
