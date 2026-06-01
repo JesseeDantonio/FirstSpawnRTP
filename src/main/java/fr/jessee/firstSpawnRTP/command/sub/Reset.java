cb614cad9d3be9700df84e134c21c7d16b028de1
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-01 18:58:55 +0200

    feat(commands): ajout de la commande `/rtp reset` pour réinitialiser les données des joueurs

--- src/main/java/fr/jessee/firstSpawnRTP/command/sub/Reset.java
+++ src/main/java/fr/jessee/firstSpawnRTP/command/sub/Reset.java
@@ -1,4 +1,58 @@
 package fr.jessee.firstSpawnRTP.command.sub;
 
-public class Reset {
+import fr.jessee.firstSpawnRTP.FirstSpawnRTP;
+import fr.jessee.firstSpawnRTP.feature.AbstractSubCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class Reset extends AbstractSubCommand {
+    @Override
+    public String getName() {
+        return "reset";
+    }
+
+    public List<String> getAliases() {
+        return List.of("");
+    }
+    @Override
+    public String getDescription() {
+        return "Reset the player's data";
+    }
+    @Override
+    public String getUsage() {
+        return "/rtp reset";
+    }
+
+    @Override
+    public void executePlayer(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player p)) return;
+
+        FirstSpawnRTP.getInstance().getPlayerRepository().getCleanTableAsync().whenComplete((result, error) -> {
+            if (error != null) {
+                FirstSpawnRTP.getInstance().getLogger().severe(error.getMessage());
+                return;
+            }
+
+            Bukkit.getScheduler().runTask(FirstSpawnRTP.getInstance(), () -> {
+                p.sendMessage(FirstSpawnRTP.getLangFile().getString("reset_success"));
+            });
+        });
+    }
+
+    @Override
+    public void executeConsole(CommandSender sender, String[] args) {
+        if (sender instanceof Player) return;
+
+        FirstSpawnRTP.getInstance().getPlayerRepository().getCleanTableAsync().whenComplete((result, error) -> {
+            if (error != null) {
+                FirstSpawnRTP.getInstance().getLogger().severe(error.getMessage());
+                return;
+            }
+
+            sender.sendMessage(FirstSpawnRTP.getLangFile().getString("reset_success"));
+        });
+    }
 }
