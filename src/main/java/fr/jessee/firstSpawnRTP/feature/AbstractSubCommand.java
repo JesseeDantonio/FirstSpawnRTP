c6a5e882cb97bcd808eb255e37924bf3f4b6d802
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-05 18:45:27 +0200

    fix(command): ajout de la méthode getPermission pour éviter d'être obligé d'ajouter une méthode inutilisé dans les classes filles.

--- src/main/java/fr/jessee/firstSpawnRTP/feature/AbstractSubCommand.java
+++ src/main/java/fr/jessee/firstSpawnRTP/feature/AbstractSubCommand.java
@@ -9,4 +9,5 @@ public abstract class AbstractSubCommand implements SubCommand {
     public List<String> getAliases() { return Collections.emptyList(); }
     public String getDescription() { return ""; }
     public String getUsage() { return ""; }
+    public String getPermission() { return ""; }
 }
