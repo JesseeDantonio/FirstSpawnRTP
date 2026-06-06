0a55595ddc3f6e6af1af9aa7a0ddc7bcaa4aeef2
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-06 09:31:35 +0200

    refactor(command): ajouter une annotation `@NonNull` pour améliorer la sécurité des paramètres

--- src/main/java/fr/jessee/firstSpawnRTP/command/RTPCommand.java
+++ src/main/java/fr/jessee/firstSpawnRTP/command/RTPCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 
@@ -47,7 +48,7 @@ public class RTPCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NonNull [] args) {
         if (sender instanceof Player p) {
             if (args.length == 0) {
                 subCommands.get("help").executePlayer(sender, args);
