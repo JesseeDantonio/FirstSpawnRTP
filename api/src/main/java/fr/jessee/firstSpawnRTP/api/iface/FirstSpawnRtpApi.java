34531db07e0872c2ee33f6c3e58564a31b7f136b
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-08 20:43:47 +0200

    feat(api,core): exposer `findSafeLocationAsync` dans l'API et rendre la méthode publique

--- api/src/main/java/fr/jessee/firstSpawnRTP/api/iface/FirstSpawnRtpApi.java
+++ api/src/main/java/fr/jessee/firstSpawnRTP/api/iface/FirstSpawnRtpApi.java
@@ -1,5 +1,7 @@
 package fr.jessee.firstSpawnRTP.api.iface;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import java.util.concurrent.CompletableFuture;
@@ -10,7 +12,17 @@ public interface FirstSpawnRtpApi {
      * Téléporte un joueur aléatoirement.
      * @param player Le joueur à téléporter
      * @param cause La cause de la téléportation
-     * @return true si la téléportation a commencé, false sinon
+     * @return CompletableFuture<Boolean>
      */
     CompletableFuture<Boolean> teleportPlayer(Player player, RtpCause cause);
+
+    /**
+     * Trouver un endroit sûr dans le monde.
+     * @param world
+     * @param center
+     * @param borderSize
+     * @param attemptsLeft
+     * @return CompletableFuture<Location>
+     */
+    CompletableFuture<Location> findSafeLocationAsync(World world, Location center, double borderSize, int attemptsLeft);
 }
